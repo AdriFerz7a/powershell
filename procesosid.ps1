b54361abb7c8570e2efc519f343a4a8831097d2c
AdriFerz7a <aferper2610@g.educaand.es>
2025-09-29 11:10:01 +0200

    V4.

--- procesosid.ps1
+++ procesosid.ps1
@@ -2,24 +2,24 @@
 ############### BUSCAR UN PROCESO #################
 ######################################################
 param (
-$proceso
+$id
 )
 $f=0
 $p=get-process
-write-host $proceso
+write-host $id
 $l=$p.length
 for ($i=0;$i -lt $l;$i++)
 {
-	if($p[$i].processName -eq $proceso)
+	if($p[$i].ID -eq $id)
 	   {
 		$f=1
 	   }
 }
 	if ($f -eq 1)
 	   {
-	 write-host "Proceso" $proceso "Encontrado"
+	 write-host "Proceso" $id "Encontrado"
 	   }
 	else
 	   {
-	 write-host "Proceso" $proceso "NO Encontrado"
+	 write-host "Proceso" $id "NO Encontrado"
 	   }
